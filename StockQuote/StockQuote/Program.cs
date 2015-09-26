6b0ff2c8b15a09ab8230f412c00ff9f5a545fd93
showtroylove <password99>
2015-09-25 23:04:33 -0400

    * App.config: * Program.cs: * App.config: * Program.cs: * Bloomberg.cs: * MainWindow.cs: * QuoteView.csproj: * SymbolLibrary.cs: * SymbolLibrary.cs: * QuoteService.cs: * SymbolLibrary.cs: * gui.stetic: * generated.cs: * MainWindow.cs: * AssemblyInfo.cs: * AssemblyInfo.cs: * Reference.cs: * Reference.cs: * Reference.svcmap: * Reference.svcmap: * StockQuoteService.xsd: * StockQuoteService.xsd: * configuration.svcinfo: * StockQuoteService.wsdl: * StockQuoteService2.xsd: * StockQuoteService3.xsd: * StockQuoteService1.xsd: * StockQuoteService.wsdl: * StockQuoteService.disco: * StockQuoteService3.xsd: * StockQuoteService2.xsd: * StockQuoteService1.xsd: * StockQuoteService.disco: * configuration91.svcinfo: * Exam70_483.Bloomberg.StockQuote.datasource:
    * libquotesvs.csproj: Added library to abstract the web service calls
      and make it easier to use the quoting service.

---
+++ StockQuote/StockQuote/Program.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+using System.Threading;
+using System.Globalization;
+using System.Runtime;
+using Bloomberg;
+
+namespace StockQuoteApp
+{
+    
+    class Program
+    {
+		public static void GetStockQuoteAsync(string symbol)
+        {
+			Bloomberg.StockQuote result = null;
+            var sw = new Stopwatch();
+			sw.Start();  
+
+            try
+            {
+				var quote = new StockQuoteServiceClient();
+				quote.InnerChannel.OperationTimeout = TimeSpan.FromMinutes(20);
+				result = quote.GetStockQuote(symbol.Trim());
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine(ex.ToString());
+            }   
+            finally 
+            {
+				sw.Stop ();                
+				var last = 0.0;
+				var qresult = double.TryParse (result.Last, out last);
+				var qlast = qresult ? last.ToString ("C") : "N/A";
+				Console.WriteLine ("{0, -40} | Symbol [{1, -6}] | Last [{2, -10}] | Elapsed time: [{3}ms]",
+					string.IsNullOrEmpty (result.Name) ? "Market Quote " : result.Name, result.Symbol, qlast, sw.ElapsedMilliseconds);            
+			}
+        }
+
+        
+        static void Main(string[] args)
+        {
+			Action<string> action = (string symbol) =>
+            {
+				GetStockQuoteAsync(symbol);                
+            };
+            
+            try
+            {
+                StartQuoteBox(action);
+                Console.WriteLine("Exiting QuoteBox...");
+            }
+			catch(Exception ex) 
+			{
+				Console.WriteLine (ex.Message);
+			}
+        }
+
+        static void DisplayQuotes(List<StockQuote> quotes)
+        {
+            foreach (var result in quotes)
+            {
+                var last = 0.0;
+                var qresult = double.TryParse(result.Last, out last);
+                var qlast = qresult ? last.ToString("C") : "N/A";
+                Console.WriteLine("{0, -40} | Symbol [{1, -6}] | Last [{2, -10}] | Elapsed time: [{3}ms]",
+                    string.IsNullOrEmpty(result.Name) ? "Market Quote " : result.Name, result.Symbol, qlast, result.ElapsedMilliseconds);            
+            }
+        }
+
+        private static void StartQuoteBox(Action<string> action)
+        {
+            var qs = new QuoteService();
+            var someValue = "-q";
+            do
+            {
+				Console.WriteLine("Enter one or more comma seperated stock symbol(s) for quote(s)\nor -nasdaq | -djia to use a default symbol set\nor type -q to exit.");
+				someValue = Console.ReadLine();
+                
+                if (someValue != "-q" && !string.IsNullOrEmpty(someValue))
+                {
+                    var quotes = qs.GetStockQuoteAsync(someValue, false);
+                    DisplayQuotes(quotes.Result);      
+                }
+
+            } while (someValue != "-q");			
+        }		
+        /*
+        private static void StartQuoteBox(Action<string> action)
+        {
+            var someValue = "-q";
+            do
+            {
+                Console.WriteLine("Enter one or more comma seperated stock symbol(s) for quote(s)\nor -nasdaq | -djia to use a default symbol set\nor type -q to exit.");
+                someValue = Console.ReadLine();
+
+                if (someValue != "-q" && !string.IsNullOrEmpty(someValue))
+                {
+                    var symbols = someValue.Split(',');
+
+                    if (someValue.StartsWith("-nasdaq"))
+                        symbols = SymbolLibrary.StockSymbols;
+                    else if (someValue.StartsWith("-djia"))
+                        symbols = SymbolLibrary.DJIA30;
+
+                    Parallel.ForEach(symbols, x => action(x));                    
+                }
+
+            } while (someValue != "-q");
+
+            Console.WriteLine("Exiting QuoteBox...");
+        }       */
+    }
+}
---
+++ StockQuote/libquotesvs/QuoteService.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Bloomberg
+{
+    public class QuoteService
+    {
+        IList<string> fqQuotes;
+        IList<string> tmpQuotes;
+
+        public QuoteService()
+        {
+            fqQuotes = new List<string>(){ "BAC", "C", "CS", "GS", "JPM", "MS" };
+        }
+
+        protected Task<IList<string>> ParseQuoteListAsync(string csvsymbols = "-djia", bool preserve = true)
+        {
+            return Task.Run(() =>
+                {
+                    var trimmedcapitalizedquotes = csvsymbols.StartsWith("-djia") ?   SymbolLibrary.DJIA30.Select(q => q.Trim()): 
+                                                   csvsymbols.StartsWith("-nasdaq") ? SymbolLibrary.NASDAQ.ToList() : 
+                                                                                      csvsymbols.Split(new char[] { ',' }).Select(q => q.Trim().ToUpper());
+                    if (preserve)
+                    {
+                        fqQuotes = trimmedcapitalizedquotes.ToList();
+                        if(null != tmpQuotes)
+                            tmpQuotes.Clear();
+                        return fqQuotes;
+                    }
+                    else
+                    {
+                        tmpQuotes = trimmedcapitalizedquotes.ToList();
+                        return tmpQuotes;
+                    }
+                });
+        }
+
+        public virtual Task<List<StockQuote>> GetStockQuoteAsync(string csvsymbols = "-djia", bool preserve = true, int timeoutInMinutes = 3)
+        {
+            return Task.Run(async () =>
+            {
+                var quotes = new List<StockQuote>();
+                try
+                {
+                    var symbols = await ParseQuoteListAsync(csvsymbols, preserve);
+
+                    foreach (var s in symbols)
+                    {
+                        var result = GetStockQuote(s, timeoutInMinutes);
+                        if(null != result)
+                            quotes.Add(result);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine(ex.ToString());
+                    throw;
+                } 
+
+                return quotes;
+            });
+        }
+
+        public StockQuote GetStockQuote(string symbol, int timeoutInMinutes = 3)
+        {
+            StockQuote quote = null;
+            var sw = new Stopwatch();     
+
+            var quotesvs = new StockQuoteServiceClient();
+            quotesvs.InnerChannel.OperationTimeout = TimeSpan.FromMinutes(timeoutInMinutes);
+
+            sw.Start();  
+            quote = quotesvs.GetStockQuote(symbol);
+            sw.Stop();
+
+            if(null != quote)
+                quote.ElapsedMilliseconds = sw.ElapsedMilliseconds.ToString();            
+
+            return quote;
+        }
+    }
+}
+
